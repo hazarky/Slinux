a18b97cb2f83d2bb3675cda13aa3a75db2d9f109
Kordell Hutchins <43355784+korryh@users.noreply.github.com>
2019-01-22 21:17:00 -0500

    Update firewall-cmd.sh

--- firewall-cmd.sh
+++ firewall-cmd.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 sudo systemctl start firewalld.service
+#For public only
 #block apipa and loopback ports
 sudo firewall-cmd --permanent --zone="public" --add-rich-rule='rule family="ipv4" source address="169.254.0.0/24" drop'
 sudo firewall-cmd --permanent --zone="public" --add-rich-rule='rule family="ipv4" source address="127.0.0.1" drop'
@@ -7,5 +8,10 @@ sudo firewall-cmd --permanent --zone="public" --add-rich-rule='rule family="ipv4
 sudo firewall-cmd --permanent --zone="public" --add-rich-rule='rule family="ipv4" port port="80" protocol="tcp" drop'
 sudo firewall-cmd --permanent --zone="public" --add-rich-rule='rule family="ipv4" port port="21" protocol="tcp" drop'
 sudo firewall-cmd --permanent --zone="public" --add-rich-rule='rule family="ipv4" port port="23" protocol="udp" drop'
-sudo systemctl restart firewalld.service
-echo "Basic Configuration is set up"
+#For Internal only
+sudo firewall-cmd --permanent --zone="internal" --add-rich-rule='rule family="ipv4" destination address="169.254.0.0/24" drop'
+sudo firewall-cmd --permanent --zone="internal" --add-rich-rule='rule family="ipv4" destination address="127.0.0.1" drop'
+
+sudo firewall-cmd --reload
+#listing all the changes for the firewall rules
+sudo firewall-cmd --list-all
